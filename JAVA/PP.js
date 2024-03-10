945c0213c03f583131ae395b7a8853c3ed4d9f66
John Doe <johndoe@email.com>
2024-03-10 16:04:37 -0700

    Begining java stuff

--- JAVA/PP.js
+++ JAVA/PP.js
@@ -1,1 +1,11 @@
-const left = document.getElementsByTagName("Left")
+const left = document.getElementsByTagName("Left");
+const img = document.getElementById('kitty');
+const right = document.getElementsByTagName('Right');
+
+
+let butlef = function (){
+    left.style.padding-left = 80%;
+};
+
+
+left.addEventListener('click', butlef());
